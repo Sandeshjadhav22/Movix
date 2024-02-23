972810a41f71f14dfc549b94633030358720cfd4
sandy <sandeshjadhav2002@gmail.com>
2024-02-24 00:10:49 +0530

    Details page half done 4:55

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import useFetch from "../../hooks/useFetch";
+import { useParams } from 'react-router-dom';
+
 import "./style.scss"
+import DetailsBanner from './detailsBanner/DetailsBanner';
 
 const Details = () => {
+ const { mediaType, id } = useParams();
+ const {data, loading} = useFetch(`/${mediaType}/${id}`)
+
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner/>
+    </div>
   )
 }
 
